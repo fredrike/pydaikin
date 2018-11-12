1835c49143b90a7dcbacdf60227e148fdd306eaa
Yari Adan Petralanda <mustang@yadan.org>
2018-11-12 16:07:09 +0100

    Wrecked up uploading 0.5 to pypi, new version required

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pydaikin',
-    version='0.5',
+    version='0.6',
     description='Python Daikin HVAC appliances interface',
     author='Yari Adan',
     author_email='mustang@yadan.org',
