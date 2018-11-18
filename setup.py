80e0106ac7174e13a7f9ad31128ea7988c02e086
Fredrik Erlandsson <fredrik.e@gmail.com>
2018-11-18 21:56:25 +0100

    version bump (support for BRP069B4X)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pydaikin',
-    version='0.7',
+    version='0.8',
     description='Python Daikin HVAC appliances interface',
     author='Yari Adan',
     author_email='mustang@yadan.org',
