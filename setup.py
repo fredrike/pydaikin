99d357f7d74b35c707df02cb68fe0c7d4f9270f2
Fredrik Erlandsson <fredrik.e@gmail.com>
2020-01-22 18:11:02 +0100

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pydaikin',
-    version='1.6.1',
+    version='1.6.2',
     description='Python Daikin HVAC appliances interface',
     author='Yari Adan',
     author_email='mustang@yadan.org',
