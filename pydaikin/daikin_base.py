b191fa12b6716cb013b8432ae0689bb84dac4037
Fredrik Erlandsson <fredrik.e@gmail.com>
2020-04-22 12:23:59 +0200

    Add support for BRP072C devices

--- pydaikin/daikin_base.py
+++ pydaikin/daikin_base.py
@@ -51,9 +51,16 @@ class Appliance:  # pylint: disable=too-many-public-methods
             DaikinBRP069,
         )
         from .daikin_skyfi import DaikinSkyFi  # pylint: disable=import-outside-toplevel
+        from .daikin_brp072c import (  # pylint: disable=import-outside-toplevel
+            DaikinBRP072C,
+        )
 
         if 'password' in kwargs and kwargs['password'] is not None:
             appl = DaikinSkyFi(device_id, session, password=kwargs['password'])
+        elif 'key' in kwargs and kwargs['key'] is not None:
+            appl = DaikinBRP072C(
+                device_id, session, key=kwargs['key'], uuid=kwargs.get('uuid'),
+            )
         else:  # special case for BRP069 and AirBase
             appl = DaikinBRP069(device_id, session)
             await appl.update_status(appl.HTTP_RESOURCES[:1])
---
+++ pydaikin/daikin_brp072c.py
@@ -0,0 +1,36 @@
+"""Pydaikin appliance, represent a Daikin device."""
+
+import logging
+from uuid import uuid3
+
+from .daikin_brp069 import DaikinBRP069
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class DaikinBRP072C(DaikinBRP069):
+    """Daikin class for BRP072Cxx units."""
+
+    def __init__(self, device_id, session=None, key=None, uuid=None):
+        """Init the pydaikin appliance, representing one Daikin AirBase (BRP15B61) device."""
+        super().__init__(device_id, session)
+        self._key = key
+        if uuid is None:
+            uuid = uuid3(__name__, 'pydaikin')
+        self._uuid = uuid.replace('-', '')
+        self._headers = {"X-Daikin-uuid": self._uuid}
+        print("BRP072C")
+
+    async def init(self):
+        """Init status."""
+        await self._get_resource(f'common/register_terminal?key={self._key}')
+        await super().init()
+
+    async def _run_get_resource(self, resource):
+        """Make the http request."""
+        async with self.session.get(
+            f'https://{self._device_ip}/{resource}', headers=self._headers, ssl=False,
+        ) as resp:
+            if resp.status == 200:
+                return self.parse_response(await resp.text())
+        return {}
