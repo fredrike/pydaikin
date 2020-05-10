60e1e7e91dd1c8e4579aadf4b5138666cdaf32f8
Fredrik Erlandsson <fredrik.e@gmail.com>
2020-05-10 20:35:45 +0200

    Version bump to 2.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pydaikin',
-    version='2.0.0',
+    version='2.0.1',
     description='Python Daikin HVAC appliances interface',
     long_description=long_description,
     long_description_content_type="text/markdown",
