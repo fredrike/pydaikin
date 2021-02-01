41481740b6ad1c4708508ed8516feca106b586cb
Fredrik Erlandsson <fredrik.e@gmail.com>
2021-02-01 13:36:58 +0100

    refactor the factory

--- pydaikin/daikin_base.py
+++ pydaikin/daikin_base.py
@@ -53,43 +53,6 @@ class Appliance(DaikinPowerMixin):  # pylint: disable=too-many-public-methods
         return sorted(list(cls.TRANSLATIONS.get(dimension, {}).values()))
 
     @staticmethod
-    async def factory(device_id, session=None, **kwargs):
-        """Factory to init the corresponding Daikin class."""
-        from .daikin_airbase import (  # pylint: disable=import-outside-toplevel,cyclic-import
-            DaikinAirBase,
-        )
-        from .daikin_brp069 import (  # pylint: disable=import-outside-toplevel,cyclic-import
-            DaikinBRP069,
-        )
-        from .daikin_brp072c import (  # pylint: disable=import-outside-toplevel,cyclic-import
-            DaikinBRP072C,
-        )
-        from .daikin_skyfi import (  # pylint: disable=import-outside-toplevel,cyclic-import
-            DaikinSkyFi,
-        )
-
-        if 'password' in kwargs and kwargs['password'] is not None:
-            appl = DaikinSkyFi(device_id, session, password=kwargs['password'])
-        elif 'key' in kwargs and kwargs['key'] is not None:
-            appl = DaikinBRP072C(
-                device_id,
-                session,
-                key=kwargs['key'],
-                uuid=kwargs.get('uuid'),
-            )
-        else:  # special case for BRP069 and AirBase
-            appl = DaikinBRP069(device_id, session)
-            await appl.update_status(appl.HTTP_RESOURCES[:1])
-            if appl.values == {}:
-                appl = DaikinAirBase(device_id, session)
-        await appl.init()
-        if not appl.values.get("mode"):
-            raise DaikinException(
-                f"Error creating device, {device_id} is not supported."
-            )
-        return appl
-
-    @staticmethod
     def parse_response(response_body):
         """Parse response from Daikin."""
         response = dict(
---
+++ pydaikin/factory.py
@@ -0,0 +1,27 @@
+"""Factory to init the corresponding Daikin class."""
+
+from .daikin_airbase import DaikinAirBase
+from .daikin_brp069 import DaikinBRP069
+from .daikin_brp072c import DaikinBRP072C
+from .daikin_skyfi import DaikinSkyFi
+
+
+async def factory(device_id, session=None, **kwargs):
+    """Factory to init the corresponding Daikin class."""
+
+    if 'password' in kwargs and kwargs['password'] is not None:
+        appl = DaikinSkyFi(device_id, session, password=kwargs['password'])
+    elif 'key' in kwargs and kwargs['key'] is not None:
+        appl = DaikinBRP072C(
+            device_id,
+            session,
+            key=kwargs['key'],
+            uuid=kwargs.get('uuid'),
+        )
+    else:  # special case for BRP069 and AirBase
+        appl = DaikinBRP069(device_id, session)
+        await appl.update_status(appl.HTTP_RESOURCES[:1])
+        if appl.values == {}:
+            appl = DaikinAirBase(device_id, session)
+    await appl.init()
+    return appl
