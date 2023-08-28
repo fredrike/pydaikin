8f576b1b41f73f7cca98ce1f9dc18021bbe1cb35
icovada <federico@tabbo.it>
2023-08-28 23:06:12 +0200

    Pass first test with CommonBasicInfo model

--- tests/test_daikin_base.py
+++ tests/test_daikin_base.py
@@ -1,24 +1,23 @@
 import pytest
 
-from pydaikin.response import parse_response
+from pydaikin.models.base import CommonBasicInfo
 
 
 @pytest.mark.parametrize(
     'body,values',
     [
         (
-            'ret=KO,type=aircon,reg=eu,dst=1',
-            dict(),
-        ),
-        (
-            'ret=OK,type=aircon,reg=eu,dst=1,ver=1_14_68,rev=C3FF8A6,pow=1',
+            "ret=OK,type=aircon,reg=eu,dst=1,ver=1_2_54,rev=203DE8C,pow=1,err=0,location=0,name=%4e%6f%74%74%65,icon=3,method=home only,port=30050,id=,pw=,lpw_flag=0,adp_kind=3,pv=3.20,cpv=3,cpv_minor=20,led=1,en_setzone=1,mac=409F38D107AC,adp_mode=run,en_hol=0,ssid1=Pinguino Curioso,radio1=-35,grp_name=,en_grp=0",
             dict(
-                type='aircon',
+                dst=True,
+                en_hol=False,
+                mac='40:9F:38:D1:07:AC',
+                name='Notte',
+                pow=True,
                 reg='eu',
-                dst='1',
-                ver='1_14_68',
-                rev='C3FF8A6',
-                pow='1',
+                rev='203DE8C',
+                type='aircon',
+                ver='1_2_54',
             ),
         ),
         (
@@ -44,4 +43,8 @@ from pydaikin.response import parse_response
     ],
 )
 def test_parse_response(body: str, values: dict):
-    assert parse_response(body) == values
+    model = CommonBasicInfo(_response=body)
+
+    values.update({"is_stale": False})
+
+    assert model.model_dump() == values
