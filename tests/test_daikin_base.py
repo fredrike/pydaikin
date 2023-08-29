6d524f2feaed06f9ab9c2b08cd27609680d40c7b
icovada <federico@tabbo.it>
2023-08-29 08:39:49 +0200

    Revert "Pass first test with CommonBasicInfo model"
    This reverts commit a8b876c42ec7d8381adf9dc3892afab56e831130.

--- tests/test_daikin_base.py
+++ tests/test_daikin_base.py
@@ -1,23 +1,24 @@
 import pytest
 
-from pydaikin.models.base import CommonBasicInfo
+from pydaikin.response import parse_response
 
 
 @pytest.mark.parametrize(
     'body,values',
     [
         (
-            "ret=OK,type=aircon,reg=eu,dst=1,ver=1_2_54,rev=203DE8C,pow=1,err=0,location=0,name=%4e%6f%74%74%65,icon=3,method=home only,port=30050,id=,pw=,lpw_flag=0,adp_kind=3,pv=3.20,cpv=3,cpv_minor=20,led=1,en_setzone=1,mac=409F38D107AC,adp_mode=run,en_hol=0,ssid1=Pinguino Curioso,radio1=-35,grp_name=,en_grp=0",
+            'ret=KO,type=aircon,reg=eu,dst=1',
+            dict(),
+        ),
+        (
+            'ret=OK,type=aircon,reg=eu,dst=1,ver=1_14_68,rev=C3FF8A6,pow=1',
             dict(
-                dst=True,
-                en_hol=False,
-                mac='40:9F:38:D1:07:AC',
-                name='Notte',
-                pow=True,
-                reg='eu',
-                rev='203DE8C',
                 type='aircon',
-                ver='1_2_54',
+                reg='eu',
+                dst='1',
+                ver='1_14_68',
+                rev='C3FF8A6',
+                pow='1',
             ),
         ),
         (
@@ -43,8 +44,4 @@ from pydaikin.models.base import CommonBasicInfo
     ],
 )
 def test_parse_response(body: str, values: dict):
-    model = CommonBasicInfo(_response=body)
-
-    values.update({"is_stale": False})
-
-    assert model.model_dump() == values
+    assert parse_response(body) == values
