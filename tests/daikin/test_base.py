7517a82bb5bf3a9eefd1caf37f948ea56d6194ed
icovada <federico@tabbo.it>
2023-08-29 16:51:03 +0200

    Test get_resource

--- tests/daikin/test_base.py
+++ tests/daikin/test_base.py
@@ -1,11 +1,20 @@
 import unittest
+from unittest.mock import patch
+
+from asyncmock import AsyncMock
 
 from pydaikin.daikin_base import Appliance
 from pydaikin.models.base import CommonBasicInfo, DaikinResponse
 
 
 class TestAppliance(unittest.IsolatedAsyncioTestCase):
-    async def test_get_resource(self):
+    @patch("pydaikin.daikin_base.ClientSession.get")
+    async def test_get_resource(self, mock_get):
+        mockresponse = AsyncMock()
+        mockresponse.text.return_value = "ret=OK,type=aircon,reg=eu,dst=1,ver=1_2_54,rev=203DE8C,pow=1,err=0,location=0,name=%4e%6f%74%74%65,icon=3,method=home only,port=30050,id=,pw=,lpw_flag=0,adp_kind=3,pv=3.20,cpv=3,cpv_minor=20,led=1,en_setzone=1,mac=409F38D107AC,adp_mode=run,en_hol=0,ssid1=Pinguino Curioso,radio1=-35,grp_name=,en_grp=0"
+        mockresponse.status = 200
+        mock_get.return_value.__aenter__.return_value = mockresponse
+
         appliance = Appliance("192.168.1.181")
 
         response = await appliance._get_resource(CommonBasicInfo)
