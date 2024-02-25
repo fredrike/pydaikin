e35c5929666118e9b2daea8eb67cc4ceacc1ad18
icovada <federico@tabbo.it>
2024-02-25 20:16:37 +0100

    Add enums

---
+++ pydaikin/enums.py
@@ -0,0 +1,14 @@
+from enum import StrEnum
+
+
+class FanModeEnum(StrEnum):
+    AUTO = "auto"
+    SILENCE = "silence"
+    S_1 = "1"
+    S_2 = "2"
+    S_3 = "3"
+    S_4 = "4"
+    S_5 = "5"
+    AUTO_LOW = "auto/low"
+    AUTO_MID = "auto/mid"
+    AUTO_HIGH = "auto/high"
