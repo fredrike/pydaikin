23b361bb752ab14b65d1fd3fab44d16c83e8af17
kingy444 <toddlesking4@hotmail.com>
2024-05-20 06:42:03 +0000

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pydaikin',
-    version='2.11.1',
+    version='2.11.2',
     description='Python Daikin HVAC appliances interface',
     long_description=long_description,
     long_description_content_type="text/markdown",
