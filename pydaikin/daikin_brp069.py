758efa3519d6eb0c777c8f5107a83cbe0ff92fb6
Brent Thew <brent.thew@gmail.com>
2024-12-28 23:21:10 +1100

    Set instance INFO_RESOURCES instead of mutating class INFO_RESOURCES for brp069

--- pydaikin/daikin_brp069.py
+++ pydaikin/daikin_brp069.py
@@ -148,10 +148,13 @@ class DaikinBRP069(Appliance):
             await self.update_status(self.HTTP_RESOURCES)
 
         if self.support_energy_consumption:
-            self.INFO_RESOURCES += [  # pylint: disable=invalid-name
-                'aircon/get_day_power_ex',
-                'aircon/get_week_power',
-            ]
+            self.INFO_RESOURCES = (  # pylint: disable=invalid-name
+                self.INFO_RESOURCES
+                + [
+                    'aircon/get_day_power_ex',
+                    'aircon/get_week_power',
+                ]
+            )
 
     async def _update_settings(self, settings):
         """Update settings to set on Daikin device."""
